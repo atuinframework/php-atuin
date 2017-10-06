75c95ef8ab237bed89d57a4b92d00e46e630decb
nyxgear <dev@nyxgear.com>
2017-10-06 12:16:35 +0200

    version typo

--- app/version.tpl.php
+++ app/version.tpl.php
@@ -1,6 +1,6 @@
 <?php
 $version = array(0, 0, 1);
-$varsion_date = array(15, 9, 2017);
+$version_date = array(15, 9, 2017);
 
 $version_string = sprintf(
     "%s.%s.%s",
@@ -11,9 +11,9 @@ $version_string = sprintf(
 
 $version_date_string = sprintf(
     "%s.%s.%s",
-    $varsion_date[0],
-    $varsion_date[1],
-    $varsion_date[2]
+    $version_date[0],
+    $version_date[1],
+    $version_date[2]
 );
 
 $version_full_string =  sprintf("%s - %s", $version_string,
